f8afc8bf8a576feba3f3216c33eddc277ac72474
Admin <nandinirsramkrishna@gmail.com>
2022-03-16 21:37:15 +0530

    scanner commit

---
+++ MR16/src/day10/Scanner.java
@@ -0,0 +1,20 @@
+package day10;
+
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+class Scannerdemo
+{
+  public static void main(String[] args)
+  {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter first number");
+    int a = s.nextInt();
+    System.out.println("Enter second number");
+    int b = s.nextInt();
+    System.out.println("Enter third number");
+    int c = s.nextInt();
+    System.out.println("All are equal : "+(a==b&&b==c&&c==a));
+    System.out.println("Any two are equal : "+(a==b||b==c||c==a));
+  }
+}
